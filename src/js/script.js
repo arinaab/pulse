7f2945c873abeaefcf93eb472f07c336614fc76a
Arina <arinameder123@gmail.com>
2021-12-27 19:00:06 +0700

    validate

--- src/js/script.js
+++ src/js/script.js
@@ -109,6 +109,7 @@ $(document).ready(function(){
       const _href = $(this).attr("href");
       $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
       return false;
-  });
-
+    });
+    
+    new WOW().init();
 });
